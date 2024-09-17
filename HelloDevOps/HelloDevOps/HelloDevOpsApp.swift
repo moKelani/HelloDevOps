ed1dd3451ef10b0e4ef74770b5274d13cbc36e5a
Mohamed Kelany <mohamed.kelany@outlook.com>
2024-09-17 21:20:30 +0300

    init project

---
+++ HelloDevOps/HelloDevOps/HelloDevOpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloDevOpsApp.swift
+//  HelloDevOps
+//
+//  Created by Mohamed Kelany on 17/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloDevOpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
